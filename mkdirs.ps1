8655ce6fbf47fd7d30e4dad49d76e96899cd24fb
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2023-11-21 22:48:36 +1300

    bring github actions up to date

---
+++ mkdirs.ps1
@@ -0,0 +1,4 @@
+if (!(Test-Path alib\obj)) { mkdir alib\obj | Out-Null }
+if (!(Test-Path alib\lib)) { mkdir alib\lib | Out-Null }
+if (!(Test-Path csvfix\obj)) { mkdir csvfix\obj | Out-Null }
+if (!(Test-Path csvfix\bin)) { mkdir csvfix\bin | Out-Null }
